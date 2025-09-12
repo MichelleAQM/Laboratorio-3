6c6cf362651e76578fa30b1dc7376ee6c8010e20
Naomi <naomi612186@gmail.com>
2025-09-11 23:30:45 -0500

    u

--- EjercicioGrupal/Menu.py
+++ EjercicioGrupal/Menu.py
@@ -3,12 +3,12 @@ from Clases import *
 def menu_juego():
     print("............| Pelea contra el Brainrot |............")
 
-    jugador = Personaje()
+    nombre = input("Ingrese el nombre de su personaje: ")
+    jugador = Personaje(nombre)
     jugador.agregar_habilidades(habilidades("Golpe fuertesini", -15))
     jugador.agregar_habilidades(habilidades("Congelar", -8))
     jugador.agregar_habilidades(habilidades("Golpe con espada arcoiris", -25))
     jugador.agregar_habilidades(habilidades("Centinela", -10))
-
     enemigoFinal = enemigosBrainrots()
     enemigoFinal.agregar_habilidades(habilidades("Golpe fuertesini", -15))
     enemigoFinal.agregar_habilidades(habilidades("Congelar", -8))
@@ -22,12 +22,15 @@ def menu_juego():
         print("3. Ver estadísticas del enemigo")
         print("4. Iniciar batalla")
         print("5. Salir")
-        opcion = int(input("Seleccione una opción: "))
-        if opcion == 1:
+
+        opcion = input("Seleccione una opción: ")
+
+        if opcion == "1":
             print("Estadísticas del Jugador")
             print("Nombre:", jugador.getNombre())
             print("Salud:", jugador.getSalud())
             print("Fuerza:", jugador.getfuerza())
+
             if jugador.equipo:
                 print("Equipo:", jugador.equipo.nombre)
                 print("Bonificación de fuerza:", jugador.equipo.bonificacion + 10)
@@ -40,27 +43,33 @@ def menu_juego():
                     print(" -", h.nombre, "(Daño:", h.daño, ")")
             else:
                 print("Habilidades: Ninguna")
-        elif opcion == 2:
+
+        elif opcion == "2":
             eq = input("Ingrese el nombre del equipo: ")
             bonif = int(input("Ingrese la bonificación base del equipo: "))
             nuevo_equipo = equipo(eq, bonif)
             jugador.asignar_equipo(nuevo_equipo)
+
         elif opcion == "3":
             print("Estadísticas del Enemigo")
             print("Nombre:", enemigoFinal.nombreEnemigo)
             print("Salud:", enemigoFinal.salud)
             print("Fuerza:", enemigoFinal.fuerza)
+
             if enemigoFinal.habilidades:
                 print("Habilidades:")
                 for h in enemigoFinal.habilidades:
                     print(" -", h.nombre, "(Daño:", h.daño, ")")
             else:
                 print("Habilidades: Ninguna")
+
         elif opcion == "4":
             batalla = Batalla(jugador, enemigoFinal)
             batalla.inicia()
+
         elif opcion == "5":
             print("Saliendo del juego...")
             break
+
         else:
             print("Opción inválida, intente de nuevo.")
